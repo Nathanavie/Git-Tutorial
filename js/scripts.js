257e6850467b5019666f5a9e57bb002bf28bd9da
Nathan Atherton <nathanatherton2@gmail.com>
2019-04-02 15:51:49 +0100

    Added event listeners for animation start times for add file, commit file, push file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,5 @@
 //Function to start when the web page is loaded
 $(document).ready(function() {
-  // $('a.create').on('click', function(event) {
-  //   event.preventDefault();
-  //   $('section').addClass("hidden");
-  //   $('#create').removeClass("hidden");
-  // });
   $('nav a').on('click', function(event) {
     event.preventDefault();
     var id = $(this).attr("id");
@@ -32,7 +27,8 @@ $(document).ready(function() {
     ev.preventDefault();
     $('section').addClass("hidden");
     $('#create').removeClass("hidden");
-    $('#animgrid').removeClass("hidden");
+    $('.top').removeClass("hidden");
+    $('.bottom').removeClass("hidden");
   });
   $('#next').click(function(ev) {
     ev.preventDefault();
@@ -55,6 +51,10 @@ $(document).ready(function() {
     $('#connectrepotext').removeClass('hidden');
     $('#connectrepotext2').css("animation-name", "fadeIn");
     $('#connectrepotext2').removeClass('hidden');
+    $('#connectrepotext3').css("animation-name", "fadeIn");
+    $('#connectrepotext3').removeClass('hidden');
+    $('#connectrepo1').css("animation-name", "fadeIn");
+    $('#connectrepo1').removeClass('hidden');
     $('.remoterepo').css("animation-name", "fadeIn");
     $('.remoterepo').removeClass('hidden');
     $('#connectrepository').addClass('hidden');
@@ -75,6 +75,10 @@ $(document).ready(function() {
     $('.profileicon').removeClass('hidden');
     $('#setuser').addClass('hidden');
     $('#createfile').removeClass("hidden");
+    $('#create').css("animation-name", "fadeOut");
+    $('#create').addClass("hidden");
+    $('#setuserinfo').css("animation-name", "fadeIn");
+    $('#setuserinfo').removeClass("hidden");
   });
   $('#createfile').click(function(ev) {
     ev.preventDefault();
@@ -85,8 +89,65 @@ $(document).ready(function() {
     $('#createfiletext').removeClass("hidden");
     $('#createfileex').css("animation-name", "fadeIn");
     $('#createfileex').removeClass("hidden");
-    $('.localreposvg').css("animation-name", "fadeIn");
-    $('.localreposvg').removeClass("hidden");
+    $('#localreposvg').css("animation-name", "fadeIn");
+    $('#localreposvg').removeClass("hidden");
     $('#createfile').addClass('hidden');
-  })
+    $('#addfile').removeClass('hidden');
+  });
+  $('#addfile').click(function(ev) {
+    ev.preventDefault();
+    $('#createfiletext').addClass("hidden");
+    $('#createfileex').addClass("hidden");
+    $('#addfiletext').css("animation-name", "fadeIn");
+    $('#addfiletext').removeClass("hidden");
+    $('#addfileex').css("animation-name", "fadeIn");
+    $('#addfileex').removeClass("hidden");
+    $('.staging').css("animation-name", "fadeIn");
+    $('.staging').removeClass('hidden');
+    $('#localtostaging').css("animation-name", "localToStage");
+    $('#localtostaging').removeClass("hidden");
+    $('#localarrow').css("animation-name", "localArrowToStage");
+    $('#localarrow').removeClass("hidden");
+    $('#addfile').addClass('hidden');
+    $('#commitfile').removeClass('hidden');
+    $('#setuserinfo').css("animation-name", "fadeOut");
+    $('#setuserinfo').addClass("hidden");
+    $('#addcommit').css("animation-name", "fadeIn");
+    $('#addcommit').removeClass("hidden");
+  });
+  $('#commitfile').click(function(ev) {
+    ev.preventDefault();
+    $('#addfileex').addClass("hidden");
+    $('#addfiletext').addClass("hidden");
+    $('#localarrow').addClass("hidden");
+    $('#committext').css("animation-name", "fadeIn");
+    $('#committext').removeClass("hidden");
+    $('#commitex').css("animation-name", "fadeIn");
+    $('#commitex').removeClass("hidden");
+    $('#commiticon').css("animation-name", "fadeIn");
+    $('#commiticon').removeClass("hidden");
+    $('#commitfile').addClass('hidden');
+    $('#pushfile').removeClass('hidden');
+  });
+  $('#pushfile').click(function(ev) {
+    ev.preventDefault();
+    $('#committext').addClass("hidden");
+    $('#commitex').addClass("hidden");
+    $('#pushtext').css("animation-name", "fadeIn");
+    $('#pushtext').removeClass("hidden");
+    $('#pushex').css("animation-name", "fadeIn");
+    $('#pushex').removeClass("hidden");
+    $('#localtostaging').addClass("hidden");
+    $('#commiticon').css("animation-name", "commitStagingtoRemote");
+    $('#staging').css("animation-name", "fileStagingtoRemote");
+    $('#stagingarrow').css("animation-name", "fadeIn");
+    $('#stagingarrow').removeClass("hidden");
+    $('#staging').removeClass("hidden");
+    $('#pushfile').addClass('hidden');
+    $('#next').removeClass('hidden');
+    $('#addcommit').css("animation-name", "fadeOut");
+    $('#addcommit').addClass("hidden");
+    $('#pushcommit').css("animation-name", "fadeIn");
+    $('#pushcommit').removeClass("hidden");
+  });
 });
